9f683a3f1979f35a3c25cbbf6d45719efdac1d81
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2014-01-01 20:14:42 +0100

    Implemented sorting, limiting and general configuration

--- testapp/client/client.js
+++ testapp/client/client.js
@@ -1,24 +1,51 @@
+function autocompleteInit (element) {
+  $(element).autocomplete({ source: []});
+}
+
+function autocompleter (config) {
+  if (typeof(config) === 'undefined'){
+    console.log('Missing required config parameter in autocompleter()');
+    return
+  }
+
+  // Build the query
+  query = {};
+  query[config['field']] = {
+    $regex: ".*" + $(config['element']).val() + ".*",
+    $options: 'i'};
+
+  // Build filtering
+  filter = {};
+  filter['limit'] = config['limit'];
+  filter['sort'] = config['sort'];
+
+  // Find all results
+  results = config['collection'].find(query, filter).fetch();
+
+  // Get the name parameter from the results
+  autocompleteResults = []
+  for (var i = results.length - 1; i >= 0; i--) {
+    autocompleteResults[i] = results[i][config['field']];
+  };
+
+  // Update the autocomplete result list
+  $(config['element']).autocomplete({ source: autocompleteResults });
+}
+
 /**
 * Template - search
 */
+Template.search.rendered = function () {
+  autocompleteInit("input#searchBox");
+}
+
 Template.search.events = {
-  'keyup input#searchBox': function (event) {
-    if (!$('input#searchBox').val())
-      return
-
-    var phrase = ".*" + $('input#searchBox').val() + ".*";
-    console.log('Searching for "' + phrase + '"');
-
-    var results = Friends.find(
-      {
-        name: { $regex: phrase, $options: 'i' }
-      }).fetch();
-
-    autocompleteResults = []
-    for (var i = results.length - 1; i >= 0; i--) {
-      autocompleteResults[i] = results[i].name;
-    };
-
-    $("input#searchBox").autocomplete({ source: autocompleteResults });
+  'keyup input#searchBox': function () {
+    autocompleter({
+      element: "input#searchBox",   // DOM identifier for the element
+      collection: Friends,          // MeteorJS collection object
+      field: "name",                // Document field name to search for
+      limit: 0,                     // Max number of elements to show
+      sort: {name: 1}});            // Sort object to filter results with
   }
 }
